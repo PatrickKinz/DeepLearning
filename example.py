590e7f8cfbba2f68783c719b5c8938bf4906c148
PatrickKinz <patrick.kinz@outlook.com>
2021-05-25 17:38:35 +0200

    tried plotting in hydrogen

--- example.py
+++ example.py
@@ -6,3 +6,19 @@ string = 'Hydrogen'
 
 for index, letter in enumerate(string):
     print((letter,index))
+
+# %% codecell
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %% codecell
+x = np.linspace(0,10,500)
+plt.plot(x, np.sin(x))
+
+%matplotlib inline
+%config InlineBackend.figure_format = 'svg'
+plt.plot(x, np.sin(x))
+import matplotlib as mpl
+mpl.style.use('ggplot')
+plt.plot(x, np.sin(x))
