8952fa2993809dcdcaa8419e3d08c13fa9f918f1
PatrickKinz <74775475+PatrickKinz@users.noreply.github.com>
2021-07-20 16:19:12 +0200

    Added gitignore for data, models and logs

---
+++ Conv2DvsConv3D.py
@@ -0,0 +1,187 @@
+import numpy as np
+from tensorflow.keras.layers import Input, Conv2D, Conv3D, DepthwiseConv2D, SeparableConv2D
+from tensorflow.keras.models import Model
+
+# Create the red, green and blue channels:
+
+red   = np.array([1]*9).reshape((3,3))
+green = np.array([100]*9).reshape((3,3))
+blue  = np.array([10000]*9).reshape((3,3))
+
+# Stack the channels to form an RGB image:
+
+img = np.stack([red, green, blue], axis=-1)
+img = np.expand_dims(img, axis=0)
+
+# Create a model that just does a Conv2D convolution:
+# %%
+inputs2D = Input((3,3,3))
+conv2D = Conv2D(filters=1,
+              strides=1,
+              padding='valid',
+              activation='relu',
+              kernel_size=2,
+              kernel_initializer='ones',
+              bias_initializer='zeros', )(inputs2D)
+model2Dconv = Model(inputs2D,conv2D)
+
+model2Dconv.summary()
+"""
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 3, 3, 3)]         0
+_________________________________________________________________
+conv2d (Conv2D)              (None, 2, 2, 1)           13
+=================================================================
+Total params: 13
+Trainable params: 13
+Non-trainable params: 0
+"""
+model2Dconv.predict(img)
+"""
+ array([[[[40404.],
+          [40404.]],
+
+         [[40404.],
+          [40404.]]]], dtype=float32)
+"""
+# %%
+depth2D = DepthwiseConv2D(
+              strides=1,
+              padding='valid',
+              activation='relu',
+              kernel_size=2,
+              depthwise_initializer='ones',
+              bias_initializer='zeros', )(inputs2D)
+model2Ddepth = Model(inputs2D,depth2D)
+
+model2Ddepth.summary()
+
+"""
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 3, 3, 3)]         0
+_________________________________________________________________
+depthwise_conv2d (DepthwiseC (None, 2, 2, 3)           15
+=================================================================
+Total params: 15
+Trainable params: 15
+Non-trainable params: 0
+"""
+model2Ddepth.predict(img)
+
+"""
+array([[[[4.e+00, 4.e+02, 4.e+04],
+         [4.e+00, 4.e+02, 4.e+04]],
+
+        [[4.e+00, 4.e+02, 4.e+04],
+         [4.e+00, 4.e+02, 4.e+04]]]], dtype=float32)
+"""
+# %%
+separable2D = SeparableConv2D(filters=1,
+              strides=1,
+              padding='valid',
+              activation='relu',
+              kernel_size=2,
+              depthwise_initializer='ones',
+              pointwise_initializer='ones',
+              bias_initializer='zeros', )(inputs2D)
+model2Dseparable = Model(inputs2D,separable2D)
+
+model2Dseparable.summary()
+
+"""
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 3, 3, 3)]         0
+_________________________________________________________________
+separable_conv2d (SeparableC (None, 2, 2, 1)           16
+=================================================================
+Total params: 16
+Trainable params: 16
+Non-trainable params: 0
+"""
+model2Dseparable.predict(img)
+
+"""
+array([[[[40404.],
+         [40404.]],
+
+        [[40404.],
+         [40404.]]]], dtype=float32)
+"""
+# %%
+
+inputs3D = Input((3,3,3,1))
+
+conv3D = Conv3D(filters=1,
+              strides=1,
+              padding='valid',
+              activation='relu',
+              kernel_size=[2,2,2],
+              kernel_initializer='ones',
+              bias_initializer='zeros', )(inputs3D)
+model3D = Model(inputs3D,conv3D)
+
+model3D.summary()
+"""
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_4 (InputLayer)         [(None, 3, 3, 3, 1)]      0
+_________________________________________________________________
+conv3d_4 (Conv3D)            (None, 2, 2, 2, 1)        9
+=================================================================
+Total params: 9
+Trainable params: 9
+Non-trainable params: 0
+"""
+model3D.predict(img)
+"""
+array([[[[[  404.],
+          [40400.]],
+
+         [[  404.],
+          [40400.]]],
+
+
+        [[[  404.],
+          [40400.]],
+
+         [[  404.],
+          [40400.]]]]], dtype=float32)
+"""
+# %%
+inputs3D = Input((3,3,3,1))
+
+conv3D = Conv3D(filters=1,
+              strides=1,
+              padding='valid',
+              activation='relu',
+              kernel_size=[2,2,3],
+              kernel_initializer='ones',
+              bias_initializer='zeros', )(inputs3D)
+model3D = Model(inputs3D,conv3D)
+
+model3D.summary()
+"""
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_3 (InputLayer)         [(None, 3, 3, 3, 1)]      0
+_________________________________________________________________
+conv3d_3 (Conv3D)            (None, 2, 2, 1, 1)        13
+=================================================================
+Total params: 13
+Trainable params: 13
+Non-trainable params: 0
+"""
+model3D.predict(img)
+"""
+array([[[[[40404.]],
+
+         [[40404.]]],
+
+
+        [[[40404.]],
+
+         [[40404.]]]]], dtype=float32)
+"""
--- Test_Exponential_2D_network.py
+++ Test_Exponential_2D_network.py
@@ -39,18 +39,32 @@ plt.plot(input_train[0,1,1,:],'o-')
 plt.plot(input_noise_train[0,1,1,:],'o')
 
 # %% norm
-def norm_signal_array(input):
+
+def norm_signal_array(input,target):
     for i in range(input.shape[0]):
         for j in range(input.shape[1]):
             for k in range(input.shape[2]):
-                temp = np.log(input[i,j,k,:])
-                input[i,j,k,:] = temp/temp[0]
-    return input
-input_noise_norm_train = norm_signal_array(input_noise_train)
+                scale = input[i,j,k,0]
+                input[i,j,k,:] =  input[i,j,k,:]/scale
+                target[i,j,k,0] =  target[i,j,k,0]/scale
+    return input,target
+input_noise_norm_train, target_train = norm_signal_array(input_noise_train, target_train)
 plt.plot(input_noise_norm_train[0,1,1,:],'o')
-input_noise_norm_test = norm_signal_array(input_noise_test)
+input_noise_norm_test, target_test = norm_signal_array(input_noise_test, target_test)
 
+"""
+input_noise_norm_train = np.log(input_noise_train)
+input_noise_norm_test = np.log(input_noise_test)
+input_norm_test = np.log(input_test)
 
+target_train[:,:,:,0] = np.log(target_train[:,:,:,0])
+target_test[:,:,:,0] = np.log(target_test[:,:,:,0])
+"""
+"""
+input_noise_norm_train = input_noise_train
+input_noise_norm_test = input_noise_test
+input_norm_test = input_test
+"""
 # %% augment data
 def augment_data(m):
     m = np.concatenate((m,np.flip(m, axis=1)), axis=0)
@@ -58,14 +72,15 @@ def augment_data(m):
     m = np.concatenate((m,np.flip(m, axis=(1,2))), axis=0)
     return m
 
-input_test = augment_data(input_test)
-input_noise_norm_test = augment_data(input_noise_norm_test)
-target_test = augment_data(target_test)
+
 #input_train = augment_data(input_train)
 input_noise_norm_train = augment_data(input_noise_norm_train)
 getsizeof(input_noise_norm_train)/(1000*1000*1000) # GB
 target_train = augment_data(target_train)
 
+input_test = augment_data(input_test)
+input_noise_norm_test = augment_data(input_noise_norm_test)
+target_test = augment_data(target_test)
 
 
 # %% Look at rotation
@@ -112,15 +127,18 @@ conv_layer_2 = keras.layers.Conv2D(n,
                   activation='relu',
                   name='conv_layer_2')(conv_layer_1)
 
+concatenate_layer = layers.Concatenate(name = 'Concat_Layer')([input_layer,conv_layer_2])
+dense_layer_1 = layers.Dense(16,name = 'Dense_1')(concatenate_layer)
+dense_layer_2 = layers.Dense(8,name = 'Dense_2')(dense_layer_1)
 
-#dense_layer_3a = layers.Dense(1, name = 'Dense_3a_S0')(conv_layer_2) # 3 outputs for S0, T2 and T2S
-dense_layer_3b = layers.Dense(1, name = 'Dense_3b_T2')(conv_layer_2) # 3 outputs for S0, T2 and T2S
-dense_layer_3c = layers.Dense(1, name = 'Dense_3c_T2S')(conv_layer_2) # 3 outputs for S0, T2 and T2S
+dense_layer_3a = layers.Dense(1, name = 'Dense_3a_S0')(dense_layer_2) # 3 outputs for S0, T2 and T2S
+dense_layer_3b = layers.Dense(1, name = 'Dense_3b_T2')(dense_layer_2) # 3 outputs for S0, T2 and T2S
+dense_layer_3c = layers.Dense(1, name = 'Dense_3c_T2S')(dense_layer_2) # 3 outputs for S0, T2 and T2S
 
 #before_lambda_model = keras.Model(input_layer, dense_layer_3, name="before_lambda_model")
 
-#Params_Layer = layers.Concatenate(name = 'Output_Params')([dense_layer_3a,dense_layer_3b,dense_layer_3c])
-Params_Layer = layers.Concatenate(name = 'Output_Params')([dense_layer_3b,dense_layer_3c])
+Params_Layer = layers.Concatenate(name = 'Output_Params')([dense_layer_3a,dense_layer_3b,dense_layer_3c])
+#Params_Layer = layers.Concatenate(name = 'Output_Params')([dense_layer_3b,dense_layer_3c])
 model_params = keras.Model(inputs=input_layer,outputs=Params_Layer,name="Params_model")
 model_params.summary()
 keras.utils.plot_model(model_params, show_shapes=True)
@@ -140,10 +158,17 @@ my_callbacks = [
 ]
 
 
-history = model_params.fit(input_noise_norm_train, target_train[:,:,:,1:], batch_size=10, epochs=50, validation_split=0.2, callbacks=my_callbacks)
-test_scores = model_params.evaluate(input_noise_norm_test, target_test[:,:,:,1:], verbose=2)
+history = model_params.fit(input_noise_norm_train, target_train[:,:,:,:], batch_size=10, epochs=50, validation_split=0.2, callbacks=my_callbacks)
+test_scores = model_params.evaluate(input_noise_norm_test, target_test[:,:,:,:], verbose=2)
 print("Test loss:", test_scores[0])
 print("Test accuracy:", test_scores[1])
+
+
+model_params.save("Model_2D_Params_before.h5")
+
+# %%
+model_params = keras.models.load_model("Model_2D_Params_before.h5")
+model_params.summary()
 
 # %%
 p = model_params.predict(input_noise_norm_test)
@@ -158,24 +183,24 @@ ax[1].imshow(target_test[Number,:,:,1], cmap='Greys')
 ax[1].title.set_text('T2')
 ax[2].imshow(target_test[Number,:,:,2], cmap='Greys')
 ax[2].title.set_text('T2S')
-#ax[3].imshow(p[Number,:,:,0], cmap='Greys')
+ax[3].imshow(p[Number,:,:,0], cmap='Greys')
 #ax[3].title.set_text('S0')
-ax[4].imshow(p[Number,:,:,0], cmap='Greys')
+ax[4].imshow(p[Number,:,:,1], cmap='Greys')
 #ax[4].title.set_text('T2')
-ax[5].imshow(p[Number,:,:,1], cmap='Greys')
+ax[5].imshow(p[Number,:,:,2], cmap='Greys')
 #ax[5].title.set_text('T2S')
 plt.show()
 # %%
 fig, axes = plt.subplots(nrows=1, ncols=3)
 ax = axes.ravel()
 ax[0].plot(target_test[Number,64,:,0])
-#ax[0].plot(p[Number,64,:,0])
+ax[0].plot(p[Number,64,:,0])
 ax[0].title.set_text('S0')
 ax[1].plot(target_test[Number,64,:,1])
-ax[1].plot(p[Number,64,:,0])
+ax[1].plot(p[Number,64,:,1])
 ax[1].title.set_text('T2')
 ax[2].plot(target_test[Number,64,:,2])
-ax[2].plot(p[Number,64,:,1])
+ax[2].plot(p[Number,64,:,2])
 ax[2].title.set_text('T2S')
 plt.show()
 
@@ -189,10 +214,112 @@ ax[1].hist(target_test[Number,:,:,1].ravel())
 ax[1].title.set_text('T2')
 ax[2].hist(target_test[Number,:,:,2].ravel())
 ax[2].title.set_text('T2S')
-#ax[3].hist(p[Number,:,:,0].ravel())
-ax[4].hist(p[Number,:,:,0].ravel())
-ax[5].hist(p[Number,:,:,1].ravel())
+ax[3].hist(p[Number,:,:,0].ravel())
+ax[4].hist(p[Number,:,:,1].ravel())
+ax[5].hist(p[Number,:,:,2].ravel())
 plt.show()
 # ax[3].hist(target_test[Number,:,:,0].ravel())
 # ax[4].hist(target_test[Number,:,:,1].ravel())
 # ax[5].hist(target_test[Number,:,:,2].ravel())
+
+
+# %%
+
+
+def simulateSignal_for_FID(tensor):
+    t=tf.constant([3,6,9,12,15,18], dtype=tf.float32)
+    t.shape
+    S0 = tensor[0]  #ln(S0)
+    T2S = tensor[1]
+    #output = S0 - tf.math.divide_no_nan(t,T2S)
+    output = S0 * tf.math.exp(-tf.math.divide_no_nan(t,T2S))
+    return output
+
+
+def simulateSignal_for_Echo_Peak_rise(tensor):
+    """x[0] = S0, x[1] = T2, x[2] = T2S   """
+    t=tf.constant([21,24,27,30,33,36,39], dtype=tf.float32)
+    S0 = tensor[0]
+    T2 = tensor[1]
+    T2S = tensor[2]
+    #output = S0 - (40.0-t)*(tf.math.divide_no_nan(1.0,T2S) - tf.math.divide_no_nan(1.0,T2)) - tf.math.divide_no_nan(t,T2)
+    output = S0 * tf.math.exp(- (40.0-t)*(tf.math.divide_no_nan(1.0,T2S) - tf.math.divide_no_nan(1.0,T2)) - tf.math.divide_no_nan(t,T2) )
+    return output
+
+def simulateSignal_for_Echo_Peak_fall(tensor):
+    """x[0] = S0, x[1] = T2, x[2] = T2S   """
+    t=tf.constant([42,45,48], dtype=tf.float32)
+    S0 = tensor[0]
+    T2 = tensor[1]
+    T2S = tensor[2]
+    #output = S0 - (t-40.0)*(tf.math.divide_no_nan(1.0,T2S) - tf.math.divide_no_nan(1.0,T2)) - tf.math.divide_no_nan(t,T2)
+    output = S0 * tf.math.exp(- (t-40.0)*(tf.math.divide_no_nan(1.0,T2S) - tf.math.divide_no_nan(1.0,T2)) - tf.math.divide_no_nan(t,T2) )
+    return output
+
+FID_Layer = layers.Lambda(simulateSignal_for_FID, name = 'FID')([dense_layer_3a,dense_layer_3c])
+Echo_Peak_rise_layer = layers.Lambda(simulateSignal_for_Echo_Peak_rise, name = 'SE_rise')([dense_layer_3a,dense_layer_3b,dense_layer_3c])
+Echo_Peak_fall_layer = layers.Lambda(simulateSignal_for_Echo_Peak_fall, name = 'SE_fall')([dense_layer_3a,dense_layer_3b,dense_layer_3c])
+output_layer = layers.Concatenate(name = 'Output_layer')([FID_Layer,Echo_Peak_rise_layer,Echo_Peak_fall_layer])
+
+
+model = keras.Model(inputs=input_layer,outputs=output_layer,name="Lambda_model")
+model.summary()
+keras.utils.plot_model(model, show_shapes=True)
+
+# %% Train full model
+model.compile(
+    loss=keras.losses.MeanSquaredError(),
+    optimizer='adam',
+    metrics=["accuracy"],
+)
+
+my_callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=2),
+    #tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
+    #tf.keras.callbacks.TensorBoard(log_dir='./logs/2021_07_15-1330')
+]
+
+history = model.fit(input_noise_norm_train, input_noise_norm_train, batch_size=20, epochs=50, validation_split=0.2, callbacks=my_callbacks)
+#test_scores = model.evaluate(input_noise_norm_test, signal_test, verbose=2)
+test_scores = model.evaluate(input_noise_norm_test, input_noise_norm_test, verbose=2)
+print("Test loss:", test_scores[0])
+print("Test accuracy:", test_scores[1])
+test_scores_params = model_params.evaluate(input_noise_norm_test, target_test, verbose=2)
+
+model_params.save("Model_2D_Params_after.h5")
+model.save("Model_2D_Full.h5")
+
+
+# %%
+p_after = model_params.predict(input_noise_norm_test)
+
+Number = 1
+
+fig, axes = plt.subplots(nrows=2, ncols=3)
+ax = axes.ravel()
+ax[0].imshow(target_test[Number,:,:,0], cmap='Greys')
+ax[0].title.set_text('S0')
+ax[1].imshow(target_test[Number,:,:,1], cmap='Greys')
+ax[1].title.set_text('T2')
+ax[2].imshow(target_test[Number,:,:,2], cmap='Greys')
+ax[2].title.set_text('T2S')
+ax[3].imshow(p_after[Number,:,:,0], cmap='Greys')
+#ax[3].title.set_text('S0')
+ax[4].imshow(p_after[Number,:,:,1], cmap='Greys')
+#ax[4].title.set_text('T2')
+ax[5].imshow(p_after[Number,:,:,2], cmap='Greys')
+#ax[5].title.set_text('T2S')
+plt.show()
+# %%
+fig, axes = plt.subplots(nrows=1, ncols=3)
+ax = axes.ravel()
+ax[0].plot(target_test[Number,64,:,0])
+ax[0].plot(p_after[Number,64,:,0])
+ax[0].title.set_text('S0')
+ax[1].plot(target_test[Number,64,:,1])
+ax[1].plot(p_after[Number,64,:,1])
+ax[1].title.set_text('T2')
+ax[2].plot(target_test[Number,64,:,2])
+ax[2].plot(p_after[Number,64,:,2])
+ax[2].title.set_text('T2S')
+plt.show()
