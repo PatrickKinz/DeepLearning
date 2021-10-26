5fdbc6c48de9df9dc4edee6cf11183fd64ecfe2a
PatrickKinz <74775475+PatrickKinz@users.noreply.github.com>
2021-10-26 20:54:31 +0200

    Increased created data

--- QSM_qBOLD_2D_create_data.py
+++ QSM_qBOLD_2D_create_data.py
@@ -4,6 +4,8 @@ from numpy.random import rand, randn, random, uniform
 from tqdm import tqdm  #for progress bar
 import SimpleITK as sitk
 import QSM_and_qBOLD_functions as QQ
+from tqdm import tqdm  #for progress bar
+
 #%%
 """
 Function to test creating 2D data of Signal with S0, T2 and T2S
@@ -205,7 +207,7 @@ examples_for_cropping()
 def create_images(seg):
     nda_seg = sitk.GetArrayViewFromImage(seg)
     count = 0 #count patches
-    for i in range(seg.GetDepth()):
+    for i in tqdm(range(seg.GetDepth())):
         #crop empty area around brain
         seg_slice = threshold_based_crop(sitk.GetImageFromArray(nda_seg[i,:,:]))
         nda_seg_slice = sitk.GetArrayFromImage(seg_slice)
@@ -221,77 +223,78 @@ def create_images(seg):
                 N_tissues=18 #Abnormal_WM not included
                 t=np.array([3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48])/1000
 
-                #a=0.5*np.ones(N_tissues) #S0 # TODO: Allow some variation
-                a=uniform(0.2,1.0,N_tissues)
-                b=random(N_tissues) #R2
-                c=random(N_tissues) #Y
-                d=random(N_tissues) #nu
-                e=random(N_tissues) #chi_nb
+                for j in range(20):
+                    #a=0.5*np.ones(N_tissues) #S0 # TODO: Allow some variation
+                    a=uniform(0.2,1.0,N_tissues)
+                    b=random(N_tissues) #R2
+                    c=random(N_tissues) #Y
+                    d=random(N_tissues) #nu
+                    e=random(N_tissues) #chi_nb
 
-                """ Calculate qBOLD and QSM for each tissue type """
-                S0 = a   #S0     = 1000 + 200 * randn(N).T
-                R2 = (30-1) * b + 1  #from 1 to 30
-                SaO2 = 0.98
-                Y  = (SaO2 - 0.01) * c + 0.01   #from 1% to 98%
-                nu = (0.1 - 0.001) * d + 0.001  #from 0.1% to 10%
-                chi_nb = ( 0.1-(-0.1) ) * e - 0.1 #from -0.1 ppb to 0.1 ppb
-                """ Special cases for air and CSF
-                # type[0] air
-                a[0]=b[0]=c[0]=d[0]=e[0] = 0
-                S0[0] = 0
-                R2[0] = 1  #to avoid infinity at 0
-                Y[0] = 0
-                nu[0] = 0
-                chi_nb[0] = 0
-                """
-                """type[3] CSF
-                #a/S0 normal
-                #for now treat CSF not as special. Later maybe add percentage of CSF to the Network, then add CSF here too
-                #T2 for CSF is between 1 and 2 seconds => R2 between 0.5 and 1
-                #b[3] =
-                #R2[3] = #smaller for CSF than rest
-                #c[3] = 0
-                #d[3] = 0
-                #e[3] = 0
-                #Y[3] = 0
-                #nu[3] = 0
-                #chi_nb[3] = 0
-                """
-                """calculate qBOLD and QSM """
-                qBOLD = QQ.f_qBOLD(S0,R2,Y,nu,chi_nb,t)
-                QSM = QQ.f_QSM(Y,nu,chi_nb)
+                    """ Calculate qBOLD and QSM for each tissue type """
+                    S0 = a   #S0     = 1000 + 200 * randn(N).T
+                    R2 = (30-1) * b + 1  #from 1 to 30
+                    SaO2 = 0.98
+                    Y  = (SaO2 - 0.01) * c + 0.01   #from 1% to 98%
+                    nu = (0.1 - 0.001) * d + 0.001  #from 0.1% to 10%
+                    chi_nb = ( 0.1-(-0.1) ) * e - 0.1 #from -0.1 ppb to 0.1 ppb
+                    """ Special cases for air and CSF
+                    # type[0] air
+                    a[0]=b[0]=c[0]=d[0]=e[0] = 0
+                    S0[0] = 0
+                    R2[0] = 1  #to avoid infinity at 0
+                    Y[0] = 0
+                    nu[0] = 0
+                    chi_nb[0] = 0
+                    """
+                    """type[3] CSF
+                    #a/S0 normal
+                    #for now treat CSF not as special. Later maybe add percentage of CSF to the Network, then add CSF here too
+                    #T2 for CSF is between 1 and 2 seconds => R2 between 0.5 and 1
+                    #b[3] =
+                    #R2[3] = #smaller for CSF than rest
+                    #c[3] = 0
+                    #d[3] = 0
+                    #e[3] = 0
+                    #Y[3] = 0
+                    #nu[3] = 0
+                    #chi_nb[3] = 0
+                    """
+                    """calculate qBOLD and QSM """
+                    qBOLD = QQ.f_qBOLD(S0,R2,Y,nu,chi_nb,t)
+                    QSM = QQ.f_QSM(Y,nu,chi_nb)
 
-                #put them in the patch
-                patch_Params = np.zeros((5,nda_seg_patch.shape[0],nda_seg_patch.shape[1]),dtype=np.float32)
-                #print(patch_Params.shape)
-                patch_qBOLD  = np.zeros((16,nda_seg_patch.shape[0],nda_seg_patch.shape[1]),dtype=np.float32)
-                patch_QSM    = np.zeros(nda_seg_patch.shape,dtype=np.float32)
-                for xx in range(patch_size):
-                    for yy in range(patch_size):
-                        type = nda_seg_patch[xx,yy]
-                        patch_Params[:,xx,yy] = np.array([a[type],b[type],c[type],d[type],e[type]])
-                        patch_qBOLD[:,xx,yy]  = qBOLD[type,:]
-                        patch_QSM[xx,yy]    = QSM[type]
-                #save 3 images
-                #print(patch_Params.dtype)
-                #outputFolder = "C:/Users/patri/Documents/Programming/Brain_Phantom/Patches/"
-                outputFolder = "../Brain_Phantom/Patches_no_air/"
-                file_number = "{0}".format(count).zfill(6)
-                sitk.WriteImage(sitk.GetImageFromArray(patch_Params),outputFolder+ "Params/Params_"+file_number+ ".TIF", useCompression=False)
-                sitk.WriteImage(sitk.GetImageFromArray(patch_qBOLD),outputFolder + "qBOLD/qBOLD_"+file_number+ ".TIF", useCompression=False)
-                sitk.WriteImage(sitk.GetImageFromArray(patch_QSM),outputFolder + "QSM/QSM_"+file_number+ ".TIF", useCompression=False)
-                count += 1
-                #patch_qBOLD
-                #patch_QSM
-        print(i, count)
+                    #put them in the patch
+                    patch_Params = np.zeros((5,nda_seg_patch.shape[0],nda_seg_patch.shape[1]),dtype=np.float32)
+                    #print(patch_Params.shape)
+                    patch_qBOLD  = np.zeros((16,nda_seg_patch.shape[0],nda_seg_patch.shape[1]),dtype=np.float32)
+                    patch_QSM    = np.zeros(nda_seg_patch.shape,dtype=np.float32)
+                    for xx in range(patch_size):
+                        for yy in range(patch_size):
+                            type = nda_seg_patch[xx,yy]
+                            patch_Params[:,xx,yy] = np.array([a[type],b[type],c[type],d[type],e[type]])
+                            patch_qBOLD[:,xx,yy]  = qBOLD[type,:]
+                            patch_QSM[xx,yy]    = QSM[type]
+                    #save 3 images
+                    #print(patch_Params.dtype)
+                    #outputFolder = "C:/Users/patri/Documents/Programming/Brain_Phantom/Patches/"
+                    outputFolder = "../Brain_Phantom/Patches_no_air_big/"
+                    file_number = "{0}".format(count).zfill(6)
+                    sitk.WriteImage(sitk.GetImageFromArray(patch_Params),outputFolder+ "Params/Params_"+file_number+ ".TIF", useCompression=False)
+                    sitk.WriteImage(sitk.GetImageFromArray(patch_qBOLD),outputFolder + "qBOLD/qBOLD_"+file_number+ ".TIF", useCompression=False)
+                    sitk.WriteImage(sitk.GetImageFromArray(patch_QSM),outputFolder + "QSM/QSM_"+file_number+ ".TIF", useCompression=False)
+                    count += 1
+                    #patch_qBOLD
+                    #patch_QSM
+        #print(i, count)
 
 
 
 
 create_images(seg)
 
-#42441    *    (5 + 16 + 1)        *30*30    *  4
-#patches *(params + qBOLD + QSM) *image size *float32 = 3.361.327.200 = 3.35GB
+#42441    *    (5 + 16 + 1)        *30*30    *  4                                  *20
+#patches *(params + qBOLD + QSM) *image size *float32 = 3.361.327.200 = 3.35GB     67.226.544.000
 #%%
 """ Loop over saved slices """
     """ Add noise to signal slices, repeat K times (total number K*?*M*N)"""
